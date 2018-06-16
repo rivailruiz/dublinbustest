23ac5a7adc9bf13e3e2766e182c66d4428859cd8
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2018-06-15 22:35:14 -0300

    remove unused code

---
+++ src/pages/SearchStop.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import dc from 'diet-cola'
+
+const Button = dc('button')(`
+  font-family: inherit;
+  font-size: inherit;
+  padding: 8px;
+  margin: 0;
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 4px;
+  appearance: none;
+  &:hover {
+    background-color: black;
+  }
+`)
